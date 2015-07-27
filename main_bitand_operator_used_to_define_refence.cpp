8d1491540aabd146a7e951f20e0278733b533241
Andrei Ehrlich <an3.e@gmx.de>
2015-07-27 11:14:44 +0200

    modified bitand-example

--- main_bitand_operator_used_to_define_refence.cpp
+++ main_bitand_operator_used_to_define_refence.cpp
@@ -5,5 +5,7 @@ int main(int argc, char** argv)
     int x = 0x33;
     int bitand y = x;
     std::cout << "x = " << std::hex << x << "; y = " << std::hex << y << std::endl;
+    x = x * 2;
+    std::cout << "x = " << std::hex << x << "; y = " << std::hex << y << std::endl;
     return 0;
 }
